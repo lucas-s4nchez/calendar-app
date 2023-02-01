9a38c9f83cd2e8fdf79908730c80592925197410
Lucas <lucassanchezdl@gmail.com>
2023-02-01 16:56:27 -0300

    feat: :sparkles: add react-big-calendar
    add react-big-calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,49 @@
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours } from "date-fns";
+
 import { Navbar } from "../components/Navbar";
+import { getMessagesEs, localizer } from "../../helpers";
+
+const events = [
+  {
+    title: "nuevo evento",
+    nota: "probando react-big-calendar",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      id: "123abc",
+      name: "Lucas",
+    },
+  },
+];
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#262254",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+    return { style };
+  };
+
   return (
     <>
       <Navbar />
+
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
   );
 };
