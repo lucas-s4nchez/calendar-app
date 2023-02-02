fe95801614383b86d98b5664d6e7606800a934c6
Lucas <lucassanchezdl@gmail.com>
2023-02-02 05:34:29 -0300

    feat: :sparkles: add validation for modal form
    add validation for modal form

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import {
+  Alert,
   Box,
   Button,
   Dialog,
@@ -14,39 +15,47 @@ import {
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import esEs from "date-fns/locale/es";
 import {
-  DateTimePicker,
   DesktopDateTimePicker,
   LocalizationProvider,
   MobileDateTimePicker,
 } from "@mui/x-date-pickers";
 
-import { format } from "date-fns";
-
 export const CalendarModal = () => {
   const [open, setOpen] = useState(true);
   const [startValue, setStartValue] = useState("");
   const [endValue, setEndValue] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
 
-  const { getFieldProps, handleSubmit, errors, touched } = useFormik({
+  const { getFieldProps, handleSubmit } = useFormik({
     initialValues: {
-      start: "",
-      end: "",
+      title: "",
+      note: "",
     },
-    validationSchema: Yup.object({
-      start: Yup.number()
-        .required("Campo requerido")
-        .min(new Date(), "Fecha inválida"),
-      end: Yup.number()
-        .min(new Date(), "Fecha inválida")
-        .required("Campo requerido"),
-    }),
+
     onSubmit: (values) => {
-      console.log(values);
+      if (!startValue || !endValue || !values.title || !values.note) {
+        setAlertMessage("Por favor, completa todos los campos del formulario");
+        return;
+      }
+      if (startValue > endValue) {
+        setAlertMessage(
+          "La fecha de finalizacion del evento debe ser mayor a la fecha de inicio del mismo"
+        );
+        return;
+      }
+
+      setAlertMessage("");
+
+      const newNote = {
+        title: values.title,
+        nota: values.note,
+        start: startValue,
+        end: endValue,
+      };
+
+      console.log(newNote);
     },
   });
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
 
   const handleClose = () => {
     setOpen(false);
@@ -59,80 +68,116 @@ export const CalendarModal = () => {
           To subscribe to this website, please enter your email address here. We
           will send updates occasionally.
         </DialogContentText>
-        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={esEs}>
-          <Box>
-            <Box sx={{ display: { xs: "block", sm: "none" } }}>
-              <MobileDateTimePicker
-                minDateTime={new Date()}
-                label="Fecha y hora de inicio"
-                value={startValue}
-                onChange={(newValue) => {
-                  setStartValue(newValue);
-                }}
-                fullWidth
-                renderInput={(params) => <TextField fullWidth {...params} />}
-              />
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+        >
+          <LocalizationProvider
+            dateAdapter={AdapterDateFns}
+            adapterLocale={esEs}
+          >
+            <Box>
+              <Box sx={{ display: { xs: "block", sm: "none" } }}>
+                <MobileDateTimePicker
+                  minDateTime={new Date()}
+                  label="Fecha y hora de inicio"
+                  value={startValue}
+                  onChange={(newValue) => {
+                    setStartValue(newValue);
+                  }}
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField
+                      fullWidth
+                      {...params}
+                      error={false}
+                      helperText="* Selecciona la fecha de inicio del evento"
+                    />
+                  )}
+                />
+              </Box>
+              <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                <DesktopDateTimePicker
+                  minDateTime={new Date()}
+                  label="Fecha y hora de inicio"
+                  value={startValue}
+                  onChange={(newValue) => {
+                    setStartValue(newValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      fullWidth
+                      {...params}
+                      error={false}
+                      helperText="* Selecciona la fecha de inicio del evento"
+                    />
+                  )}
+                />
+              </Box>
             </Box>
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
-              <DesktopDateTimePicker
-                minDateTime={new Date()}
-                label="Fecha y hora de inicio"
-                value={startValue}
-                onChange={(newValue) => {
-                  setStartValue(newValue);
-                }}
-                renderInput={(params) => <TextField fullWidth {...params} />}
-              />
+            <Box>
+              <Box sx={{ display: { xs: "block", sm: "none" } }}>
+                <MobileDateTimePicker
+                  minDateTime={startValue !== "" ? startValue : new Date()}
+                  label="Fecha y hora de finalización"
+                  value={endValue}
+                  onChange={(newValue) => {
+                    setEndValue(newValue);
+                  }}
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField
+                      fullWidth
+                      {...params}
+                      error={false}
+                      helperText="* Selecciona la fecha de finalización del evento"
+                    />
+                  )}
+                />
+              </Box>
+              <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                <DesktopDateTimePicker
+                  minDateTime={startValue !== "" ? startValue : new Date()}
+                  label="Fecha y hora de finalización"
+                  value={endValue}
+                  onChange={(newValue) => {
+                    setEndValue(newValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      fullWidth
+                      {...params}
+                      error={false}
+                      helperText="* Selecciona la fecha de finalización del evento"
+                    />
+                  )}
+                />
+              </Box>
             </Box>
-          </Box>
-          <Box>
-            <Box sx={{ display: { xs: "block", sm: "none" } }}>
-              <MobileDateTimePicker
-                minDateTime={startValue !== "" ? startValue : new Date()}
-                label="Fecha y hora de finalización"
-                value={endValue}
-                onChange={(newValue) => {
-                  setEndValue(newValue);
-                }}
-                fullWidth
-                renderInput={(params) => <TextField fullWidth {...params} />}
-              />
-            </Box>
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
-              <DesktopDateTimePicker
-                minDateTime={startValue !== "" ? startValue : new Date()}
-                label="Fecha y hora de finalización"
-                value={endValue}
-                onChange={(newValue) => {
-                  setEndValue(newValue);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    fullWidth
-                    error={endValue < startValue}
-                    helperText="cambia la fecha pibe"
-                    {...params}
-                  />
-                )}
-              />
-            </Box>
-          </Box>
-        </LocalizationProvider>
+          </LocalizationProvider>
 
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
+          <TextField
+            label="Título"
+            type="text"
+            fullWidth
+            {...getFieldProps("title")}
+            helperText="* Título del evento"
+          />
+          <TextField
+            label="Nota"
+            type="text"
+            fullWidth
+            {...getFieldProps("note")}
+            helperText="* Información adicional sobre el evento"
+          />
+          {!!alertMessage && <Alert severity="error">{alertMessage}</Alert>}
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit">Subscribe</Button>
+          </DialogActions>
+        </Box>
       </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleClose}>Subscribe</Button>
-      </DialogActions>
     </Dialog>
   );
 };
